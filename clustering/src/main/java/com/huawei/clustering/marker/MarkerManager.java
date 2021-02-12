0f6530af6198453fd770380915e982a13bacfe30
Alexey Khaiminov <a.khaiminov@corp.mail.ru>
2021-02-12 20:36:53 +0300

    added marker manager

---
+++ clustering/src/main/java/com/huawei/clustering/marker/MarkerManager.java
@@ -0,0 +1,64 @@
+package com.huawei.clustering.marker;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.huawei.hms.maps.HuaweiMap;
+import com.huawei.hms.maps.model.BitmapDescriptor;
+
+import java.util.List;
+
+public final class MarkerManager<T> {
+
+    public interface IconGenerator<T> {
+
+        @NonNull BitmapDescriptor getMarkerIcon(@NonNull T marker);
+
+    }
+
+    public interface LatLngGenerator<T> {
+
+        double getLang(T item);
+
+        double getLng(T item);
+
+    }
+
+    public interface ItemClickListener<T> {
+
+        void onItemClick(T item);
+
+    }
+
+    private final MarkerRenderer<T> renderer;
+
+    public MarkerManager(
+        @NonNull HuaweiMap huaweiMap,
+        @NonNull IconGenerator<T> iconGenerator,
+        @NonNull LatLngGenerator<T> latLngGenerator
+    ) {
+        renderer = new MarkerRenderer<>(huaweiMap, iconGenerator, latLngGenerator);
+    }
+
+    public void setOnItemClickListener(@Nullable ItemClickListener<T> listener) {
+        renderer.setOnClickListener(listener);
+    }
+
+    public void addItems(@NonNull List<T> items) {
+        renderer.add(items);
+    }
+
+    public void removeItems(@NonNull List<T> items) {
+        renderer.remove(items);
+    }
+
+    public void updateItems(@NonNull List<T> items) {
+        renderer.update(items);
+    }
+
+    public void clear() {
+        renderer.clear();
+    }
+
+
+}
---
+++ clustering/src/main/java/com/huawei/clustering/marker/MarkerRenderer.java
@@ -0,0 +1,148 @@
+package com.huawei.clustering.marker;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.huawei.hms.maps.HuaweiMap;
+import com.huawei.hms.maps.model.BitmapDescriptor;
+import com.huawei.hms.maps.model.LatLng;
+import com.huawei.hms.maps.model.Marker;
+import com.huawei.hms.maps.model.MarkerOptions;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public final class MarkerRenderer<T> {
+
+    private final Map<T, Marker> itemToMarkers = new HashMap<>();
+    private final Map<Marker, T> markerToItems = new HashMap<>();
+
+    @NonNull
+    private final HuaweiMap huaweiMap;
+    @NonNull
+    private final MarkerManager.IconGenerator<T> iconGenerator;
+    @NonNull
+    private final MarkerManager.LatLngGenerator<T> latLngGenerator;
+
+    private MarkerManager.ItemClickListener<T> clickListener = null;
+
+    public MarkerRenderer(
+            @NonNull HuaweiMap huaweiMap,
+            @NonNull MarkerManager.IconGenerator<T> iconGenerator,
+            @NonNull MarkerManager.LatLngGenerator<T> latLngGenerator
+    ) {
+        this.huaweiMap = huaweiMap;
+        this.iconGenerator = iconGenerator;
+        this.latLngGenerator = latLngGenerator;
+        this.huaweiMap.setOnMarkerClickListener(new HuaweiMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                if (clickListener == null)
+                    return false;
+
+                final T item = markerToItems.get(marker);
+                if (item != null) {
+                    clickListener.onItemClick(item);
+                    return true;
+                }
+
+                return false;
+            }
+        });
+    }
+
+
+    void setOnClickListener(@Nullable MarkerManager.ItemClickListener<T> onClickListener) {
+        clickListener = onClickListener;
+    }
+
+    void add(@NonNull Collection<T> items) {
+        add(items, true);
+    }
+
+    private void add(@NonNull Collection<T> items, boolean isAnim) {
+        final HashSet<T> toAdd = new HashSet<>(items);
+        toAdd.removeAll(this.itemToMarkers.keySet());
+
+        //animate
+        for (T item: toAdd) {
+            final BitmapDescriptor markerIcon = iconGenerator.getMarkerIcon(item);
+            int FOREGROUND_MARKER_Z_INDEX = 1;
+            final Marker markerToAdd = huaweiMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(
+                            latLngGenerator.getLang(item),
+                            latLngGenerator.getLng(item)
+                        ))
+                    .icon(markerIcon)
+                    .alpha(0.0F)
+                    .zIndex(FOREGROUND_MARKER_Z_INDEX))
+                    ;
+            if (isAnim)
+                animateMarkerAppearance(markerToAdd);
+            else
+                markerToAdd.setAlpha(1.0f);
+
+            this.itemToMarkers.put(item, markerToAdd);
+            this.markerToItems.put(markerToAdd, item);
+        }
+    }
+
+    void remove(@NonNull Collection<T> items) {
+        remove(items, true);
+    }
+
+    private void remove(@NonNull Collection<T> items, boolean isAnim) {
+        final HashSet<T> toRemove = new HashSet<>(this.itemToMarkers.keySet());
+        toRemove.retainAll(items);
+
+        //animate
+        for (T item: toRemove) {
+            Marker markerToRemove = itemToMarkers.get(item);
+            if (markerToRemove == null)
+                return;
+
+            if (isAnim) {
+                animateWithMarkerRemove(markerToRemove);
+            } else
+                markerToRemove.remove();
+
+            this.markerToItems.remove(markerToRemove);
+            this.itemToMarkers.remove(item);
+        }
+    }
+
+    void update(@NonNull Collection<T> items) {
+        remove(items, false);
+        add(items, false);
+    }
+
+    void clear() {
+        remove(this.itemToMarkers.keySet());
+        this.itemToMarkers.clear();
+        this.markerToItems.clear();
+    }
+
+    private void animateMarkerAppearance(@NonNull Marker marker) {
+        ObjectAnimator.ofFloat(marker, "alpha", 1.0F).start();
+    }
+
+    private void animateWithMarkerRemove(@NonNull final Marker marker) {
+        int BACKGROUND_MARKER_Z_INDEX = 0;
+        marker.setZIndex(BACKGROUND_MARKER_Z_INDEX);
+        ObjectAnimator fadeAnimator = ObjectAnimator.ofFloat(marker, "alpha", 0.0F);
+        fadeAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                marker.remove();
+            }
+        });
+        fadeAnimator.start();
+    }
+
+}
